310dbc890c7a65d70b923217ef80d1bbf9c5ce5d
Agustina-Burelli <aburelli@fi.uba.ar>
2026-09-24 23:40:35 +0000

    avance servicio de estados y conecte con MySQL

--- app_backend/services/estados.py
+++ app_backend/services/estados.py
@@ -1,3 +1,8 @@
+from db import query_one, execute
+from datetime import datetime, timezone, timedelta
+
+ZONA_CLUB = timezone(timedelta(hours=-3))
+
 def validar_transicion(estado_actual, estado_nuevo, inicio, fin, ahora):
     #validar que los estados sean válidos
     estados_validos = ["confirmada", "cancelada", "finalizada"]
@@ -18,4 +23,73 @@ def validar_transicion(estado_actual, estado_nuevo, inicio, fin, ahora):
         return ahora >= fin
     
     return False
+
+
+def obtener_estado_reserva(id_reserva):
+    sql = """
+        SELECT id, estado, fecha_hora_inicio, fecha_hora_fin
+        FROM reservas
+        WHERE id = %s
+        """
+    #%s es identificador por separa para que MYSQL procese correctamente el parametro
+    reserva = query_one(sql,(id_reserva,))
+    #se guardan los datos de la reserva en un diccionario, sino devuelve none "flechita hacia arriba"
+    return reserva
+
+def cambiar_estado_reserva(id_reserva, estado_nuevo):
+
+    estados_validos = [
+        "confirmada",
+        "cancelada",
+        "finalizada"
+    ]
+
+    if estado_nuevo not in estados_validos:
+        return {
+            "error": "El estado solicitado NO es válido"
+        }
+
+    reserva = obtener_estado_reserva(id_reserva)
+
+    if reserva is None:
+        return {
+            "error": "Reserva NO encontrada"
+        }
     
+    estado_actual = reserva["estado"]
+    inicio = reserva["fecha_hora_inicio"]
+    fin = reserva["fecha_hora_fin"]
+
+    inicio = inicio.replace(tzinfo=ZONA_CLUB)
+    fin = fin.replace(tzinfo=ZONA_CLUB)
+
+    ahora = datetime.now(ZONA_CLUB)
+
+    permitido = validar_transicion(
+        estado_actual,
+        estado_nuevo, 
+        inicio, 
+        fin, 
+        ahora
+    )
+    if not permitido:
+        return {
+            "error": "la transición de estado no está permitida"
+        }
+
+    if estado_actual == estado_nuevo:
+        return {
+            "mensaje": "la reserva ya tiene ese estado"
+        }
+
+    sql = """
+        UPDATE reservas
+        SET estado = %s
+        WHERE id = %s
+    """
+
+    execute(sql, (estado_nuevo, id_reserva))
+
+    return {
+        "mensaje": f"Estado de reserva actualizado correctamente"
+        }
